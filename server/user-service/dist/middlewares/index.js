630d0366781e95fca11d1d50353385fcbc17caaf
Nicolae Basso <nickbassotrue@gmail.com>
2024-06-08 23:32:22 +0300

    update gitignores

--- server/user-service/dist/middlewares/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = exports.authenticate = void 0;
-var authMiddleware_1 = require("./authMiddleware");
-Object.defineProperty(exports, "authenticate", { enumerable: true, get: function () { return authMiddleware_1.authenticate; } });
-var loggerMiddleware_1 = require("./loggerMiddleware");
-Object.defineProperty(exports, "logger", { enumerable: true, get: function () { return loggerMiddleware_1.logger; } });
--- server/user-service/dist/middlewares/loggerMiddleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (req, res, next) => {
-    console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
-    next();
-};
-exports.logger = logger;
